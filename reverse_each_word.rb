5792b0ae2017a7129d6095d11d42cd07d5a6973f
aurel fontaine <ajfame11@gmail.com>
2020-03-31 19:02:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,13 +2,13 @@ def reverse_each_word(sentence)
   array = sentence.split("")
   new_sentence = array.each do |word|
     word.reverse!
-  end
   return new_sentence.join("")
 end
+
+
 def reverse_each_word(sentence)
   array = sentence.split("")
   new_sentence = array.collect |word|
     word.reverse!
-  end
   return new_sentence.join("")
 end

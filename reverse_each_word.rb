37bdb89d9e272dfff3182e1a7d5662536049fb54
aurel fontaine <ajfame11@gmail.com>
2020-03-31 18:31:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence)
-  sentence.split
-  reverse_each_word.each do |word|
-    puts "word".reverse!
+  sentence = sentence.split("")
+  reverse_sentence.each do |word|
     
   end
 end
